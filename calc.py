39244ba0d61f5f8080ca8c4777900159c3f6b711
Josh Orton <jeo27@bath.ac.uk>
2017-08-14 18:29:37 +0100

    Adding power functionality

--- calc.py
+++ calc.py
@@ -1,2 +1,19 @@
 def multiply(x,y):
-    return x*y;
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def power(x,y):
+    if (y==0):
+        return 1
+
+    elif (y<2):
+         return x
+
+    else: return x * power(x,y-1)
+
+x = power(4,2);
+print(power(4,3))
+print(power(4,0))
+print(x)
